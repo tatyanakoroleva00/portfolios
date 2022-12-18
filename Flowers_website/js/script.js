db33f1d3a184b6ba78781f7173af8e033fd64d7d
Tatiana <tatyanakoroleva00@mail.ru>
2022-12-18 13:06:34 +0300

    carusel added

--- Flowers_website/js/script.js
+++ Flowers_website/js/script.js
@@ -26,3 +26,40 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+/*SLIDER*/
+
+
+
+function addSlides(m) {
+  let photos = document.querySelectorAll('.photo');
+  let leftButton = document.querySelector('.left');
+  let rightButton = document.querySelector('.right');
+
+  if (m === 1) {
+    photos[0].style.display = 'none';
+    photos[1].style.display = 'none';
+    photos[2].style.display = 'none';
+    photos[3].style.display = 'none';
+    photos[4].style.display = 'block';
+    photos[5].style.display = 'block';
+    photos[6].style.display = 'block';
+    photos[7].style.display = 'block';
+    leftButton.style.display = 'block';
+    rightButton.style.display = 'none';
+} else {
+    photos[0].style.display = 'block';
+    photos[1].style.display = 'block';
+    photos[2].style.display = 'block';
+    photos[3].style.display = 'block';
+    photos[4].style.display = 'none';
+    photos[5].style.display = 'none';
+    photos[6].style.display = 'none';
+    photos[7].style.display = 'none';
+    leftButton.style.display = 'none';
+    rightButton.style.display = 'block';
+}
+  }
+  
+  
